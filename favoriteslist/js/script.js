c055800b4daec5bf29ab900c9f5171fc0f9b9fe1
Jonas Nettli <jonas.nettli@gmail.com>
2024-01-18 13:47:14 +0100

    Played a bit more

--- favoriteslist/js/script.js
+++ favoriteslist/js/script.js
@@ -1,16 +1,32 @@
-import products from "./data/products.js";
 import productsData from "./data/products.js";
 
 const productsContainer = document.querySelector(".products-container");
+const savedFavProducts = JSON.parse(window.localStorage.getItem("favorites"));
+console.log("savedFavProducts: ", savedFavProducts)
 
 for (let i = 0;  i < productsData.length; i++) {
+    let cssClass = "";
+
+    const existObject = savedFavProducts.find(function(favHeart){
+        return parseInt(favHeart.id) === productsData[i].id
+    });
+
+    if(existObject) {
+        cssClass = "active-heart";
+    }
+
+    console.log("existObject: ", existObject)
+
     productsContainer.innerHTML += `
     <div class="product">
         <img src="img/gnomechild.png" alt="Gnome Child">
         <h3>${productsData[i].name}</h3>
         <h4>${productsData[i].price}</h4>
         <span class="heart-container">
-            <svg class="heart"
+            <svg class="heart ${cssClass}"
+                data-id="${productsData[i].id}";
+                data-name="${productsData[i].name}";
+                data-price="${productsData[i].price}";
                 xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
             </svg>
         </span>
@@ -20,7 +36,7 @@ for (let i = 0;  i < productsData.length; i++) {
 
 const favHeart = document.querySelectorAll(".heart");
 
-
+/*
 for (let j = 0; j < favHeart.length; j++) {
     favHeart[j].addEventListener('click', function(){
         if(favHeart[j].classList.contains("active-heart")) {
@@ -30,6 +46,27 @@ for (let j = 0; j < favHeart.length; j++) {
         }
     });
 }
+*/
+
+let saveFav = [];
+
+for (let x = 0; x < favHeart.length; x++) {
+    favHeart[x].addEventListener("click", function() {
+        this.classList.toggle("active-heart");
+
+        let productSaved = {
+            id: this.dataset.id,
+            name: this.dataset.name,
+            price: this.dataset.price
+        }
+        saveFav.push(productSaved);
+        timeToSave(saveFav);
+    });
+}
+
+function timeToSave(gang) {
+    window.localStorage.setItem("favorites", JSON.stringify(gang));
+}
 
 const unsortedArray = [1, 2, 3, 51523, -12938192, 5312.12312, 22, 0, -1, -2, -3];
 
@@ -37,12 +74,9 @@ const unsortedArray = [1, 2, 3, 51523, -12938192, 5312.12312, 22, 0, -1, -2, -3]
 // Sorting numbers from lowest to highest
 unsortedArray.sort(function(a, b){return a-b});
 
-console.log(unsortedArray);
 
 const minArrayItem = unsortedArray[0];
-console.log(minArrayItem);
 const maxArrayItem = unsortedArray[unsortedArray.length - 1];
-console.log(maxArrayItem);
 
 
 let positiveEvenArray = [];
