06b9a888acbdd446b615eaac66250424860af936
Jonas Nettli <jonas.nettli@gmail.com>
2024-01-18 10:56:43 +0100

    Added more javascript

---
+++ favoriteslist/js/data/products.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        id: 0,
+        name: "Date Night with Chris",
+        price: 20,
+    },
+    {
+        id: 1,
+        name: "Sensual Reading of Shrek",
+        price: 20000,
+    },
+    {
+        id: 2,
+        name: "Very Legal Drugs",
+        price: 69420,
+    },
+    {
+        id: 3,
+        name: "Soultrain Ticket",
+        price: 2,
+    },
+    {
+        id: 4,
+        name: "My Faith in Humanity",
+        price: 0,
+    }
+];
