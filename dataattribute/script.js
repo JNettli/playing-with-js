3b2f1d2463e2416691412b8986bdd67e0720305b
Jonas Nettli <jonas.nettli@gmail.com>
2024-01-25 14:24:26 +0100

    Fixed dropdown menu with css

--- dataattribute/script.js
+++ dataattribute/script.js
@@ -5,16 +5,12 @@ const menuItems = document.querySelectorAll(".menuItem");
 
 for(let i = 0; i < menuItems.length; i++) {
     menuItems[i].onmouseenter = function() {
-        console.log(menuItems[i].children[0])
         menuItems[i].children[0].style.display = "block";
     }
     menuItems[i].onmouseleave = function() {
         menuItems[i].children[0].style.display = "none";
     }
 }
-
-
-
 
 coolBTN.onclick = function() {
     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs", "_self");
