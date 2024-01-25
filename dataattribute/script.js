f66631b3db3fef348f99ecf1125d022d766386d5
Jonas Nettli <jonas.nettli@gmail.com>
2024-01-25 15:15:45 +0100

    Added some buttons and age check

--- dataattribute/script.js
+++ dataattribute/script.js
@@ -2,6 +2,36 @@ const article = document.getElementById("coolDiv");
 const coolBTN = document.querySelector("#gang-BTN");
 const coolMenu = document.getElementById("thaMenu");
 const menuItems = document.querySelectorAll(".menuItem");
+const enterBTN = document.getElementById("enter");
+const safeBTN = document.getElementById("safety");
+const ageInput = document.querySelector("#age");
+
+enterBTN.onclick = function() {
+    document.querySelector(".naughtyEnter").style.display = "none";
+}
+
+safeBTN.onclick = function() {
+    window.open("https://www.youtube.com/watch?v=pxn0wL_uSm4", "_self");
+}
+
+enterBTN.addEventListener("click", function() {
+    const age = ageInput.value;
+    const isAgeOk = isOldEnough(age);
+    if(isAgeOk) {
+        document.querySelector(".naughtyEnter").style.display = "none";
+    } else {
+        alert("You are too young, bucko!")
+    }
+})
+
+function isOldEnough(ageToCheck) {
+    if(ageToCheck >= 18) {
+        return true;
+    }
+    return false;
+}
+
+
 
 for(let i = 0; i < menuItems.length; i++) {
     menuItems[i].onmouseenter = function() {
@@ -11,6 +41,7 @@ for(let i = 0; i < menuItems.length; i++) {
         menuItems[i].children[0].style.display = "none";
     }
 }
+
 
 coolBTN.onclick = function() {
     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs", "_self");
